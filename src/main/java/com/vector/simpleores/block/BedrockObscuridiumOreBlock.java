417e44eab250370f37104ee3911c5387316a0f04
Vitor <victor2001v10@gmail.com>
2025-11-15 18:56:29 +0000

    nothing

--- src/main/java/com/vector/simpleores/block/BedrockObscuridiumOreBlock.java
+++ src/main/java/com/vector/simpleores/block/BedrockObscuridiumOreBlock.java
@@ -1,8 +1,12 @@
 package com.vector.simpleores.block;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
@@ -10,6 +14,8 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import com.vector.simpleores.item.ModItems;
+
+import java.util.List;
 
 public class BedrockObscuridiumOreBlock extends Block {
 
@@ -27,6 +33,13 @@ public class BedrockObscuridiumOreBlock extends Block {
         }
         world.removeBlock(pos, false);
         super.onBlockExploded(state, world, pos, explosion);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, Item.TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+        pTooltipComponents.add(Component.translatable("bedrockobscuridiumoreblock").withStyle(ChatFormatting.AQUA));
+
+        super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
     }
 
     @Override
